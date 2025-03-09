4d07c50befa2a38d8a21a8785ef0ec6e42a16540
justindayane <dayanebobo@gmail.com>
2025-03-09 12:32:20 -0400

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Justin Dayane  Gbadamassi on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
