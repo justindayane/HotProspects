b19dfa593f7675eb138a14f0616367703c223de9
justindayane <dayanebobo@gmail.com>
2025-03-09 14:45:29 -0400

    First time trying result containing error or returned value

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -14,33 +14,44 @@ import SwiftUI
 - Give each view a string tag and then use that for the state property
  */
 struct ContentView: View {
-    @State private var selectedTab = "One"
+    @State private var output = ""
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            VStack{
-                Spacer()
-                Text("Tab 1")
-                Spacer()
-                Button("Move to tabe 2") {
-                    selectedTab = "two"
-                    print(selectedTab)
-                }
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
-                .background(.red)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                Spacer()
+        Text(output)
+            .task {
+                await fetchReadings()
             }
-            .tabItem {
-                Image(systemName: "star")
-            }
-            .tag("one")
-            Text("tab 2")
-                .tabItem {
-                    Image(systemName: "plus")
-                    Text("two")
-                }
-                .tag("two")
+    }
+    
+    func fetchReadings() async {
+        // We create a task object that will carry out the task
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "I found \(readings.count) readings"
+        }
+        
+        // We then observe the result of the task performed. Could be what we returned or an error
+            
+        // No need to use try here because the results has th error in it as well
+        let result = await fetchTask.result
+        
+        // Now that we have the result, we have 2 options to handle it
+        
+        // Option 1
+//        do {
+//            output = try result.get()
+//        } catch {
+//            output = "Error: - \(error.localizedDescription)"
+//        }
+        
+        // Option 2
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let failure):
+            output = "Error - \(failure.localizedDescription)"
         }
     }
 }
