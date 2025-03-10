9a7ef2f107ba843ea0e8d9d7e3f12a296b8c34a1
justindayane <dayanebobo@gmail.com>
2025-03-10 16:42:56 -0400

    Trying out swipeActions

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,52 +8,49 @@
 import SwiftUI
 
 /* Notes
-- In case you were tempted to mix tabView and navigationStack
- Remember that TabView has to be the parent
-- Use tags to identify the tabs and improvise with their names
-- Give each view a string tag and then use that for the state property
+ 
  */
 struct ContentView: View {
-    @State private var output = ""
+    @State private var backgroundColor = Color.blue
+    @State private var messages = 0
+    let people = ["Mayoly Santana", "Rami Arias", "Masiel Alvarez", "Oliver Munoz"]
     
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
+        NavigationStack {
+            List (people, id:\.self) { person in
+                Text(person)
+                    .swipeActions {
+                        Button("Send Message", systemImage: "paperplane") {
+                            print("You just sent a message to \(person)")
+                            messages += 1
+                        }
+                        .tint(.blue)
+                    }
+                    .swipeActions {
+                        Button("Delete", systemImage: "trash", role: .destructive) {
+                            print("You just deleted \(person)")
+                        }
+                    }
+                    .swipeActions(edge: .leading) {
+                        Button("Pin", systemImage: "pin") {
+                            print("You just pinned \(person)")
+                        }
+                        .tint(.orange)
+                    }
+                
             }
+            .toolbar {
+                Button("Add", systemImage: "person.circle") {
+                    
+                }
+            }
+            
+            
+            Text("\(messages) messages sent so far")
+                
+        }
     }
     
-    func fetchReadings() async {
-        // We create a task object that will carry out the task
-        let fetchTask = Task {
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "I found \(readings.count) readings"
-        }
-        
-        // We then observe the result of the task performed. Could be what we returned or an error
-            
-        // No need to use try here because the results has th error in it as well
-        let result = await fetchTask.result
-        
-        // Now that we have the result, we have 2 options to handle it
-        
-        // Option 1
-//        do {
-//            output = try result.get()
-//        } catch {
-//            output = "Error: - \(error.localizedDescription)"
-//        }
-        
-        // Option 2
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let failure):
-            output = "Error - \(failure.localizedDescription)"
-        }
-    }
 }
 
 #Preview {
