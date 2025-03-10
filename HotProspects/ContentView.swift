8ab34037971c14a04aa9adb9c2a8421d605607c3
justindayane <dayanebobo@gmail.com>
2025-03-10 17:27:10 -0400

    Adding dependencies and trying it out

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import UserNotifications
+import SamplePackage
 
 /* Notes
  Notifications:
@@ -14,41 +14,16 @@ import UserNotifications
  
  */
 struct ContentView: View {
+    let possibleNumbers = 1...60
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted() // This is from the package
+        let strings = selected.map(String.init)
+        return strings.formatted()
+    }
     
     var body: some View {
         VStack {
-            Button("Request Permission") {
-                // Permission request code => First
-                // Request Permission for alert, sound and badge
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All Set!")
-                    } else if let error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("Schedule Notification") {
-                // Notification schedule + request code => Second
-                // content is what should be shown
-                // trigger determines when notification should be shown based on time, date or geo location
-                // request combines content and trigger and also adds a UUID so you can edit it later
-                // As a beginner, the easier notification to start with is the UNTimeIntervalNotification
-                
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the Kion"
-                content.subtitle = "He looks hungry"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                // chose a random identifier
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                // Now we can add our notification request
-                UNUserNotificationCenter.current().add(request)
-            }
+            Text(results)
         }
     }
     
