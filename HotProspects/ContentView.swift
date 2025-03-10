dcf8ed7ca2adb249a0260ade0b1a919c4318d9a9
justindayane <dayanebobo@gmail.com>
2025-03-10 17:11:44 -0400

    Trying out local notifications

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,48 +6,49 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 /* Notes
+ Notifications:
+ Here we focus on Local Notification, Not Push notifications that required goign through APNS
  
  */
 struct ContentView: View {
-    @State private var backgroundColor = Color.blue
-    @State private var messages = 0
-    let people = ["Mayoly Santana", "Rami Arias", "Masiel Alvarez", "Oliver Munoz"]
     
     var body: some View {
-        NavigationStack {
-            List (people, id:\.self) { person in
-                Text(person)
-                    .swipeActions {
-                        Button("Send Message", systemImage: "paperplane") {
-                            print("You just sent a message to \(person)")
-                            messages += 1
-                        }
-                        .tint(.blue)
+        VStack {
+            Button("Request Permission") {
+                // Permission request code => First
+                // Request Permission for alert, sound and badge
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All Set!")
+                    } else if let error {
+                        print(error.localizedDescription)
                     }
-                    .swipeActions {
-                        Button("Delete", systemImage: "trash", role: .destructive) {
-                            print("You just deleted \(person)")
-                        }
-                    }
-                    .swipeActions(edge: .leading) {
-                        Button("Pin", systemImage: "pin") {
-                            print("You just pinned \(person)")
-                        }
-                        .tint(.orange)
-                    }
-                
-            }
-            .toolbar {
-                Button("Add", systemImage: "person.circle") {
-                    
                 }
             }
             
-            
-            Text("\(messages) messages sent so far")
+            Button("Schedule Notification") {
+                // Notification schedule + request code => Second
+                // content is what should be shown
+                // trigger determines when notification should be shown based on time, date or geo location
+                // request combines content and trigger and also adds a UUID so you can edit it later
+                // As a beginner, the easier notification to start with is the UNTimeIntervalNotification
                 
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the Kion"
+                content.subtitle = "He looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                // chose a random identifier
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                // Now we can add our notification request
+                UNUserNotificationCenter.current().add(request)
+            }
         }
     }
     
